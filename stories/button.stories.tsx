6e38c1d6e92d8de3bf27d6138f8a5b4ac8c958ce
FRANK GO <frankgo81@hotmail.com>
2022-09-20 23:35:41 +0800

    button

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -1,16 +1,30 @@
 import { Story, Meta } from '@storybook/react';
 
-import { Button, ButtonProps } from '@tarojs/components';
-import { PropsWithChildren } from 'react';
+import { Button, ButtonProps, Text } from '@tarojs/components';
+import { Children, CSSProperties, PropsWithChildren } from 'react';
+import './button.scss';
 
 const meta: Meta = {
   title: 'Button',
   component: Button,
 };
 
-export const Base = Button as Story<PropsWithChildren<ButtonProps>>;
+export const Base = (props: ButtonProps) => {
+  // const itemStyle: CSSProperties = {
+  //   height: '100px',
+  //   textAlign: 'center',
+  //   lineHeight: '100px',
+  //   color: '#fff',
+  // };
+  return (
+    <Button {...props} className="b">
+      <Text className="t">{Base.args.children}</Text>
+    </Button>
+  );
+};
+
 Base.args = {
-  children: 'Hello',
+  children: '一键登录确认',
 };
 
 export default meta;
